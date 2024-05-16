8887afb282df1228c6ebc114819bb674101e51c2
nikolaz.90 <nikolazstacey@hotmail.co.uk>
2024-05-16 19:31:56 +0200

    comment

--- src/pages/SingleItemPage.js
+++ src/pages/SingleItemPage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import {useGlobalContext} from '../context'
+import { useGlobalContext } from '../context'
 import noImageLogo from '../photos/logos/No_image_available.svg.png'
 
 function SingleItemPage() {
-  const {id} = useParams()
-  const {loading, dataFromPam} = useGlobalContext()
+  const { id } = useParams()
+  const { loading, dataFromPam } = useGlobalContext()
 
-  const {paintings = {reflections:[],isolations:[],perceptions:[]}, screens, prints} = dataFromPam 
-  const {reflections, isolations, perceptions} = paintings
+  const { paintings = { reflections: [], isolations: [], perceptions: [] }, screens, prints } = dataFromPam
+  const { reflections, isolations, perceptions } = paintings
 
   const allArtwork = [...reflections || [], ...isolations || [], ...perceptions || [], ...screens || [], ...prints || []]
   const singleArtwork = allArtwork.filter((item) => item.id === parseInt(id))[0]
@@ -16,11 +16,12 @@ function SingleItemPage() {
   return (
     <main>
       {loading ? <h5 className='loading'>LOADING... {id}</h5> : <article className='single-artwork-container'>
-        <img className='single-artwork-img' src={singleArtwork.image_url === "" ? noImageLogo : singleArtwork.image_url} alt={singleArtwork.title}/>
+        <img className='single-artwork-img' src={singleArtwork.image_url === "" ? noImageLogo : singleArtwork.image_url} alt={singleArtwork.title} />
         <div className='single-artwork-info'>
           <h2>{singleArtwork.title}</h2>
           <p><small>{singleArtwork.materials} {singleArtwork.dimensions !== 'n/a' ? `- ${singleArtwork.dimensions}` : ""}</small></p>
           <p>{singleArtwork.description}</p>
+          {/* <p><small>{singleArtwork.price}</small></p> */}
         </div>
       </article>}
     </main>
